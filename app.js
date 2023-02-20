d524c0a7032e944a97eb8c24e82382cacd1c2e40
Anderson dos Santos Maciel <CIANDT\amaciel@lnb029686cps.ciandt.global>
2023-02-20 09:51:41 -0300

    NodeJS

---
+++ app.js
@@ -0,0 +1,107 @@
+    const express = require("express");
+    const { randomUUID } = require("crypto");
+    const fs = require("fs");
+
+    const app = express();
+
+    app.use(express.json());
+
+    let products = [];
+
+    fs.readFile("products.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        products = JSON.parse(data);
+    }
+    });
+
+/**
+ * POST => Inserir um dado
+ * GET => Buscar um dado
+ * PUT => Alterar um dado
+ * DELETE => Remover um dado
+ */
+
+/**
+ * Body => Sempre que eu quiser dados para minha aplicação
+ * Params => /product/254566565656
+ * Query => /product?id=25689632323&value685968965656
+ */
+
+    app.post("/products", (request, response) => {
+    // Nome e preço => name e price
+        
+    const { name, price } = request.body;
+
+    const product = {
+    name,
+    price,
+    id: randomUUID(),
+    };
+
+    products.push(product);
+    
+    createProductFile();
+
+    /**fs.writeFile("products.json", JSON.stringify (products), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Produto inserido");
+        }
+    });
+    */
+    return response.json(products);   
+    });
+
+    app.get("/products", (request, response) => {
+        return response.json(products);
+
+    });
+
+    app.get("/products/:id",(request, response) => { 
+        const { id } = request.params;
+        const product = products.find((product) => product.id === id);
+        return response.json(product);
+    });
+
+    app.put("/products/:id", (request, response) => {
+    const { id } = request.params;
+    const { name, price } = request.body;
+   
+const productIndex = products.findIndex(product => product.id === id);
+products[productIndex] = {
+    ...products[productIndex],
+    name,
+    price,
+};
+
+productFile();
+
+return response.json({ message: "Produto alterado com sucesso"});
+});
+
+app.delete("/products/:id", (request, response) => {
+const { id } = request.params;
+
+const productIndex = products.findIndex((product) => product.id === id);
+
+products.splice(produtoIndex, 1);
+
+productFile();
+
+return response.json({ message: "Produto removido com sucesso!"});
+});
+
+function productFile() {
+ fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Produto inserido");
+        }
+});
+}
+
+    app.listen(4002, () => console.log("Servidor está rodando na porta 4002"));
